26a34be755e7a34622f334d400cf7abe44742a56
storrer <storrer.chris@gmail.com>
2022-12-29 22:39:20 -0800

    Initial commit adds one solution and a links file

---
+++ links.md
@@ -0,0 +1,1 @@
+- Palidrome Number https://leetcode.com/problems/palindrome-number/description/
---
+++ palindrome-number.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ * 
+ */
+ var isPalindrome = function(x) {
+    // If input is negative or ends in zero return false
+    if (x < 0) {
+        return false;
+    }
+    else if ((x >= 0) && (x <= 9)) {
+        return true; // But if the number is between zero and ten return true
+    }
+    else if (x % 10 === 0) {
+        return false;
+    }
+    
+
+    // Reverse the input integer and determine if palidrome
+    if (x === reverseInteger(x)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+let reverseInteger = function(x) {
+    let reverse = 0;    
+    for (let y = x; y > 0;) {
+        let lastDigit = y % 10;
+        reverse = (reverse * 10) + lastDigit;
+        y = y - lastDigit;
+        y = y/10;
+    }
+    return reverse;
+};
